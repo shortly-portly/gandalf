de55d50f75dbabded101240f8476dff1f4d4aadb
David Simmons <davidsimmons@davids-mbp.home>
2020-08-23 21:10:14 +0100

    Add automatic table head columns names for table widget.

--- src/cljs/gandalf/view.cljs
+++ src/cljs/gandalf/view.cljs
@@ -3,15 +3,8 @@
    [gandalf.widget :as widget]
    [re-frame.core :as rf]))
 
-;; (defn index-page []
-;;   [:section.section>div.container>div.content
-;;    (when-let [resource @(rf/subscribe [:view])
-;;               view (:view resource)
-;;               data (:data resource)]
-;;        [:h1 "wibble"])])
-
 (defn index-page []
   [:section.section>div.container>div.content
    (when-let [view @(rf/subscribe [:view])]
-
-     [widget/widget view])])
+     [:div
+      [widget/widget view]])])
--- src/cljs/gandalf/widget.cljs
+++ src/cljs/gandalf/widget.cljs
@@ -1,10 +1,29 @@
 (ns gandalf.widget
-  (:require [reagent.core :as r]
+  (:require [clojure.string]
+            [reagent.core :as r]
             [re-frame.core :as rf]))
 
 (defn build-path
   ([root path] (if (vector? path) path (conj root path)))
   ([root index path] (if (vector? path) path (conj root index path))))
+
+(defn field-path-to-string
+  "Returns a string version of the provided keyword path."
+  [path]
+  (-> (name path)
+      (clojure.string/replace "_" " ")
+      (clojure.string/capitalize)))
+
+
+(defn label-for
+  "Returns the label for a the given `field`.
+
+  `field` is a map that may contain an optional :label key - in which
+  case this value is used. If not then the field name is the string version of
+  the field path with the first letter capitalised and any underscores replaced
+  with spaces."
+  [field]
+  (if (:label field) (:label field) (field-path-to-string(:path field))))
 
 (defmulti widget :type)
 
@@ -12,39 +31,28 @@
   (let [value (r/atom @(rf/subscribe [:data path]))]
     (fn []
       [:div
-      [:label label]
       [:span @value]])))
+
 
 (defmethod widget :table [view]
   (fn []
     (let [root   [(:path view)]
           fields (:fields view)
           item-count @(rf/subscribe [:item-count root])]
-      [:div
-       (for [index (range item-count)]
-       (for [field fields]
-         (let [data-path   (build-path root index (:path field))
-               widget-data (assoc field :path data-path)]
-
-           ^{:key (:path field)}  [widget widget-data])))])))
-
-
-;;                widget-data (assoc field :data-path data-path :schema-path schema-path)]
-;;            ^{:key (:path field)} [widget widget-data]))
-
-;;         (for [action actions]
-;;           ^{:key (:dispatch action)} [widget action])])))
-
-
-;; (defmethod widget :collection [collection-data]
-;;   (fn []
-;;     (let [root (:data-path collection-data)
-;;           fields (:fields collection-data)
-;;           item-count @(rf/subscribe [:item-count root])]
-;;       [:div
-;;        (for [index (range item-count)]
-;;          (for [field fields]
-;;            (let [data-path (build-path root index (:path field))
-;;                  schema-path (build-path root (:path field))
-;;                  widget-data (assoc field :data-path data-path :schema-path schema-path)]
-;;          ^{:key (:path field)} [widget widget-data])))])))
+      [:table.table
+        [:thead
+         [:tr
+          (for [field fields]
+            ^{:key field}
+            [:th
+             (label-for field)])]]
+       [:tbody
+        (for [index (range item-count)]
+          ^{:key index}
+          [:tr
+           (for [field fields]
+             ^{:key field}
+             [:td
+              (let [data-path   (build-path root index (:path field))
+                    widget-data (assoc field :path data-path)]
+                [widget widget-data])])])]])))
